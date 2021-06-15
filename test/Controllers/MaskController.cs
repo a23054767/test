b31458c8675da297a31d0d69e05bbd025672c338
i5-10400 <i5-10400@DESKTOP-IMNNUJ0>
2021-06-15 17:34:35 +0800

    加入專案檔案。

---
+++ test/Controllers/MaskController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using test.Models;
+using System.Net.Http;
+
+namespace test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaskController : ControllerBase
+    {
+        private readonly MaskService _maskService;
+
+        public MaskController(MaskService maskService)
+        {
+            _maskService = maskService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var maskCount = await _maskService.GetMaskInfo();
+                return Ok(maskCount.FirstOrDefault());
+            }
+            catch (HttpRequestException ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
